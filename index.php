77d3e49c8b863cb046f2518b2b869edcb99bedfa
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-06 18:10:42 +0000

    Homepage triptych image template

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content">
-	<h1>Hello Kasper</h1>
-	<section>
+	<section class="triptych-section">
+		<?php perch_content("Homepage Triptych"); ?>
 	</section>
 </main>
 <?php perch_layout('main-footer'); ?>
