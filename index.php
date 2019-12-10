2b809d5dd0884df293a5e7398d46cc6efa8f8d11
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-10 17:29:33 +0000

    Showreel player template

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('homepage-header'); ?>
 <main class="main-content homepage-main-content">
-		<?php perch_content("Homepage Triptych"); ?>
-		<?php perch_content("Introduction Text"); ?>
+	<?php perch_content("Homepage Triptych"); ?>
+	<?php perch_content("Introduction Text"); ?>
+	<?php perch_layout('showreel-player'); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
---
+++ perch/templates/layouts/showreel-player.php
@@ -0,0 +1,3 @@
+<div class="showreel-player">
+  <img src="/images/showreel-player-arrow.svg" alt="Showreel player arrow">
+</div>
