f12cb67c6588c9d5ace4ba28156997b03ec1956b
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-16 18:56:37 +0000

    Photography page title template

--- photography.php
+++ photography.php
@@ -1,9 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content photography-main-content" id="main-content">
-	<section>
-	  <?php perch_content('Placeholder'); ?>
-	</section>
+	<?php perch_content("Page Title"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
