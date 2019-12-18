e1963e4dbbf0a0e4b9bb539a24c70402fb3c6f33
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-18 21:46:45 +0000

    Installations page

---
+++ installations.php
@@ -0,0 +1,7 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('main-header'); ?>
+<main class="main-content installations-main-content" id="main-content">
+	<?php perch_content("Page Title"); ?>
+</main>
+<?php perch_layout('main-footer'); ?>
+<!-- <?php PerchUtil::output_debug(); ?> -->
