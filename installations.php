4ec2b38f43b3b43631723febacab01136e0aa506
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-19 16:21:51 +0000

    Installations layout template

--- installations.php
+++ installations.php
@@ -2,6 +2,7 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content installations-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
+	<?php perch_layout("installations-links"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
--- privacy-and-cookie-policy.php
+++ privacy-and-cookie-policy.php
@@ -1,7 +1,7 @@
-main<?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
+<?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
 <?php perch_layout('main-header'); ?>
 	<div class="page-wrapper">
-		<main class="legals privacy-policy">
+		<main class="legals privacy-policy" id="main-content">
         <?php perch_content('Privacy And Cookie Policy'); ?>
 		</main>
 		<?php perch_layout('main-footer'); ?>
