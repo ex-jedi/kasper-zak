30d886cd5a6e80d5a191c6961f27d653f04e70b4
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-20 18:23:29 +0000

    About me contact form

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
 <main class="main-content about-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
 	<?php perch_content("Image And Text"); ?>
+	<?php perch_content("General Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
