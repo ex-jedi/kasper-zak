1a2980bf9d79e36ea22572cb2f98ca3abe39824e
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-20 18:11:24 +0000

    About me title

--- about.php
+++ about.php
@@ -2,6 +2,7 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content about-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
+	<?php perch_content("Image And Text"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
