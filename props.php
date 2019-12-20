3424be4044066e55373c687e293863bd7b5c3f4a
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-20 14:45:17 +0000

    Props gallery template

--- props.php
+++ props.php
@@ -3,5 +3,7 @@
 <main class="main-content props-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
 	<?php perch_content("Page Subtitles"); ?>
+	<?php perch_content("Props Gallery"); ?>
+	<?php perch_content("Props Text"); ?>
 <?php perch_layout('main-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
