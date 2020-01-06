c7b71890a0ac582eb91d0dc4020c29f4ffcdc5aa
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-06 15:12:43 +0000

    Adding 404 page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
+<?php perch_layout('main-header'); ?>
+	<div class="page-wrapper">
+		<main class="main-content four-oh-four-main-content">
+        <?php perch_content('Terms And Conditions'); ?>
+		</main>
+		<?php perch_layout('main-footer'); ?>
