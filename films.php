d027459b8594db6ecf29420dbfd184c819b56f26
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-07 17:11:19 +0000

    Invisible header and footer for films page

--- films.php
+++ films.php
@@ -1,9 +1,9 @@
 <?php include('perch/runtime.php'); ?>
-<?php perch_layout('main-header'); ?>
+<?php perch_layout('invisible-header'); ?>
 <main class="main-content films-main-content" id="main-content">
 	<section>
 	  <?php perch_content('Films Page Videos'); ?>
 	</section>
 </main>
-<?php perch_layout('main-footer'); ?>
+<?php perch_layout('invisible-footer'); ?>
 <!-- <?php PerchUtil::output_debug(); ?> -->
---
+++ perch/templates/layouts/invisible-footer.php
@@ -0,0 +1,9 @@
+<footer class="invisible-footer">
+</footer>
+</div> <!-- wrapper div -->
+<!-- TODO: Change Vue to production  version -->
+<script type="text/javascript" src="/scripts/minified/vue.js" ></script>
+<!-- General JS file -->
+<script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
+</body>
+</html>
