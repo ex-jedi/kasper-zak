4c69ed6f82c947886869099e5088c5e16b8e3e08
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-09 17:23:11 +0000

    Video controller with placeholder animaition

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -17,9 +17,9 @@ const app = new Vue({
 
 // ********** Video control containers **********
 
-const videoContainer = document.getElementById('videoContainer');
 const video = document.getElementById('video');
-const videoControls = document.getElementById('video-controls');
+const videoTitle = document.getElementById('video-title');
+const videoCounter = document.getElementById('video-counter');
 
 // * Hide default video controls
 video.controls = false;
@@ -27,32 +27,33 @@ video.controls = false;
 // ********** Video control buttons **********
 
 const playpause = document.getElementById('playpause');
-const stop = document.getElementById('stop');
-const mute = document.getElementById('mute');
-const volinc = document.getElementById('volinc');
-const voldec = document.getElementById('voldec');
-const progress = document.getElementById('progress');
-const progressBar = document.getElementById('progress-bar');
-const fullscreen = document.getElementById('fs');
 
 // ********** Video control events **********
 
 // * Play pause function
-
 function playAndPause() {
   if (video.paused || video.ended) {
     video.controls = true;
     video.play();
+    playpause.classList.add('hide-play-button');
+    videoTitle.classList.add('hide-video-title');
+    videoCounter.classList.add('hide-video-counter');
   } else {
     video.pause();
   }
 }
+
 playpause.addEventListener('click', playAndPause);
 
 // * Detect if video is paused
-
 function isPaused() {
-  if (!video.seeking) video.controls = false;
+  // Hide controls unless user is seeking
+  if (!video.seeking) {
+    video.controls = false;
+    playpause.classList.remove('hide-play-button');
+    videoTitle.classList.remove('hide-video-title');
+    videoCounter.classList.remove('hide-video-counter');
+  }
 }
 
 video.addEventListener('pause', isPaused);
