a568d3746dd2ab6aea90660094aa43a29eac694c
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-09 14:01:02 +0000

    Working play/pause video control

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -10,3 +10,35 @@ const app = new Vue({
     categoryReveal: false,
   },
 });
+
+// *=========================================
+// ** Video Controls  **
+// *=========================================
+
+// ********** Video control containers **********
+
+const videoContainer = document.getElementById('videoContainer');
+const video = document.getElementById('video');
+const videoControls = document.getElementById('video-controls');
+
+// * Hide default video controls
+video.controls = false;
+
+// ********** Video control buttons **********
+
+const playpause = document.getElementById('playpause');
+const stop = document.getElementById('stop');
+const mute = document.getElementById('mute');
+const volinc = document.getElementById('volinc');
+const voldec = document.getElementById('voldec');
+const progress = document.getElementById('progress');
+const progressBar = document.getElementById('progress-bar');
+const fullscreen = document.getElementById('fs');
+
+// ********** Video control events **********
+
+// * Play pause
+playpause.addEventListener('click', function(e) {
+  if (video.paused || video.ended) video.play();
+  else video.pause();
+});
