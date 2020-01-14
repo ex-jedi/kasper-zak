3bbe707ef3b1854b3b73344b87724f0c23cc53a6
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-14 17:05:28 +0000

    Experimenting with greensock animations

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -1,3 +1,9 @@
 // *=========================================
 // ** Animations  **
 // *=========================================
+
+gsap.fromTo(
+  '.triptych-image-one',
+  { xPercent: -100 },
+  { duration: 1, delay: 1, xPercent: 0, ease: 'back.out(1)' },
+);
--- scripts/minified/homepage-min.js
+++ scripts/minified/homepage-min.js
@@ -1,2 +1,2 @@
-
+gsap.fromTo(".triptych-image-one",{xPercent:-100},{duration:1,delay:1,xPercent:0,ease:"back.out(1)"});
 //# sourceMappingURL=homepage-min.js.map
