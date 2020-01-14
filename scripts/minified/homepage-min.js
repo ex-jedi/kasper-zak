635ca4ea61b744e148761033025156de80648b91
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-14 18:02:28 +0000

    Revides triptych gsap timeline

--- scripts/minified/homepage-min.js
+++ scripts/minified/homepage-min.js
@@ -1,2 +1,2 @@
-gsap.fromTo(".triptych-image-one",{xPercent:-100},{duration:1,delay:1,xPercent:0,ease:"back.out(1)"});
+const tlOne=gsap.timeline({defaults:{duration:.6,ease:Back.easeOut.config(1)}});tlOne.fromTo(".triptych-image-one",{xPercent:-100},{xPercent:0},"+=1").fromTo(".triptych-image-three",{xPercent:100},{xPercent:0},"-=0.2").fromTo(".triptych-image-two",{yPercent:100,opacity:0},{yPercent:0,opacity:1,ease:Back.easeOut.config(2)},"-=0.2");
 //# sourceMappingURL=homepage-min.js.map
