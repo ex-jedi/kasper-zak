372bfe5b1cd891bd524730b900f2e4c827c600bc
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-14 18:48:02 +0000

    More tweaks

--- scripts/js/homepage.js
+++ scripts/js/homepage.js
@@ -11,9 +11,16 @@ const tlOne = gsap.timeline({
 tlOne
   .fromTo('.triptych-image-one', { xPercent: -100 }, { xPercent: 0 }, '+=1')
   .fromTo('.triptych-image-three', { xPercent: 100 }, { xPercent: 0 }, '-=0.2')
+  .addLabel('togetherness', '-=.5')
   .fromTo(
     '.triptych-image-two',
-    { yPercent: 100, opacity: 0 },
-    { yPercent: 0, opacity: 1, ease: Back.easeOut.config(2) },
-    '-=0.2',
+    { opacity: 0 },
+    { opacity: 1, duration: 1.5, ease: 'none' },
+    'togetherness',
+  )
+  .fromTo(
+    '.triptych-image-two',
+    { yPercent: 100 },
+    { yPercent: 0, duration: 2, ease: 'elastic.out(1, 0.3)' },
+    'togetherness',
   );
