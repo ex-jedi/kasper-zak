da20f545439d48d8517bb33eda15ac74352ba904
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-10 19:33:15 +0000

    Play and pause working

--- scripts/js/films.js
+++ scripts/js/films.js
@@ -7,8 +7,8 @@
 
 // ********** Video control containers **********
 
-// Targets all video wrappers for control functions
-const videoWrapper = document.querySelectorAll('.video-wrapper');
+const videoPlayButton = document.querySelectorAll('.play-button');
+console.log('Play Button', videoPlayButton);
 
 // Targents all video to remove controls on page load
 const video = document.querySelectorAll('.video');
@@ -24,26 +24,41 @@ video.forEach(function(elem) {
 // ********** Video control events **********
 
 // * Play pause function
-function playAndPause() {
-  let actualVideo = this.childNodes[3];
-  console.log('Video', actualVideo);
+function playAndPause(e) {
+  // Searcing for the things
+  let videoTitle = this.parentNode.childNodes[1];
+  // console.log('Video Title', videoTitle);
 
-  let videoTitle = this.childNodes[1];
-  // console.log('Title', videoTitle);
-
-  let videoIcon = this.childNodes[5];
+  let videoIcon = this;
   // console.log('Icon', videoIcon);
 
-  if (actualVideo.paused || actualVideo.ended) {
-    actualVideo.play();
-    actualVideo.controls = true;
+  let videoActual = this.parentNode.childNodes[3];
+  // console.log('Video', videoActual);
+
+  // Play pause and controls
+  if (videoActual.paused || videoActual.ended) {
+    videoActual.controls = true;
+    videoActual.play();
     videoIcon.classList.add('hide-play-button');
     videoTitle.classList.add('hide-video-title');
     videoCounter.classList.add('hide-video-counter');
-  } else {
-    actualVideo.pause();
+  } else if (!video.seeking) {
+    videoActual.pause();
   }
 }
 
-// Adding controls function to video wrappers
-videoWrapper.forEach(item => item.addEventListener('click', playAndPause));
+videoPlayButton.forEach(item => item.addEventListener('click', playAndPause));
+
+// // * Detect if video is paused
+// function isPaused() {
+//   // Hide controls unless user is seeking
+//   console.log('Paused', this);
+//   if (!video.seeking) {
+//     video.controls = false;
+
+//   }
+// }
+
+// video.forEach(element => {
+//   element.addEventListener('pause', isPaused);
+// });
