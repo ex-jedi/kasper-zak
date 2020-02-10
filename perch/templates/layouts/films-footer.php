0b3c04ab2e6bd7370819723e119ee626d1598695
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-10 13:21:35 +0000

    Films JS in its own file

--- perch/templates/layouts/films-footer.php
+++ perch/templates/layouts/films-footer.php
@@ -4,7 +4,8 @@
 <!-- TODO: Change Vue to production  version -->
 <script type="text/javascript" src="/scripts/minified/vue.js" ></script>
 <!-- General JS file -->
-<script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
+<script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Universal JS files -->
 <!-- Films JS File  -->
+<script src="/scripts/minified/films-min.js"></script>
 </body>
 </html>
