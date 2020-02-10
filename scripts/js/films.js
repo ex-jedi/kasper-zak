8a5f79c354c948c4aab78e0a4437ee91bf3e755e
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-10 15:41:20 +0000

    Setting up vue app for video page

--- scripts/js/films.js
+++ scripts/js/films.js
@@ -5,46 +5,59 @@
 // ** Video Controls  **
 // *=========================================
 
+const filmsApp = new Vue({
+  el: '#vue-films-wrapper',
+  data: {
+    switch: false,
+  },
+  methods: {
+    pluggedIn() {
+      alert('hi');
+    },
+  },
+});
+
 // ********** Video control containers **********
 
-const video = document.getElementById('video');
-const videoTitle = document.getElementById('video-title');
-const videoCounter = document.getElementById('video-counter');
+// var video = document.querySelectorAll('.video');
+// console.log(video);
+// const videoTitle = document.querySelectorAll('.video-title');
+// const videoCounter = document.getElementById('video-counter');
 
-// * Hide default video controls
-video.controls = false;
+// // * Hide default video controls
+// video.controls = false;
 
-// ********** Video control buttons **********
+// // ********** Video control buttons **********
 
-const playpause = document.getElementById('playpause');
+// const playpause = document.querySelectorAll('.video-player-icon');
 
-// ********** Video control events **********
+// // ********** Video control events **********
 
-// * Play pause function
-function playAndPause(e) {
-  console.log(e);
-  if (video.paused || video.ended) {
-    video.controls = true;
-    video.play();
-    playpause.classList.add('hide-play-button');
-    videoTitle.classList.add('hide-video-title');
-    videoCounter.classList.add('hide-video-counter');
-  } else {
-    video.pause();
-  }
-}
+// // * Play pause function
+// function playAndPause(e) {
+//   console.log(e);
+//   if (video.paused || video.ended) {
+//     this.video.controls = true;
+//     this.video.play();
+//     this.playpause.classList.add('hide-play-button');
+//     this.videoTitle.classList.add('hide-video-title');
+//     this.videoCounter.classList.add('hide-video-counter');
+//   } else {
+//     this.video.pause();
+//   }
+// }
 
-playpause.addEventListener('click', playAndPause);
+// video.forEach(item => item.addEventListener('click', playAndPause));
 
-// * Detect if video is paused
-function isPaused() {
-  // Hide controls unless user is seeking
-  if (!video.seeking) {
-    video.controls = false;
-    playpause.classList.remove('hide-play-button');
-    videoTitle.classList.remove('hide-video-title');
-    videoCounter.classList.remove('hide-video-counter');
-  }
-}
+// // * Detect if video is paused
+// function isPaused() {
+//   // Hide controls unless user is seeking
+//   if (!video.seeking) {
+//     video.controls = false;
+//     playpause.classList.remove('hide-play-button');
+//     videoTitle.classList.remove('hide-video-title');
+//     videoCounter.classList.remove('hide-video-counter');
+//   }
+// }
 
-video.addEventListener('pause', isPaused);
+// video.addEventListener('pause', isPaused);
