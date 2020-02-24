4fde4d7ef7bbc127962134262e9c416c3e6f6acb
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-24 20:32:36 +0000

    Improved films.js

--- scripts/js/films.js
+++ scripts/js/films.js
@@ -22,17 +22,17 @@ video.forEach(elem => {
 
 // * Play pause function
 function playAndPause() {
-  const parentElement = this.parentNode;
-  const videoIcon = this;
-  const videoTitle = parentElement.childNodes[1];
-  const videoActual = parentElement.childNodes[3];
-  const videoCounter = parentElement.childNodes[7];
+  const videoWrapper = this.parentElement;
+  const videoActual = videoWrapper.querySelector('.video');
+  const videoTitle = videoWrapper.querySelector('.video-title');
+  const videoButton = this;
+  const videoCounter = videoWrapper.querySelector('.video-counter');
 
   // Play pause and controls
   if (videoActual.paused || videoActual.ended) {
     videoActual.controls = true;
     videoActual.play();
-    videoIcon.classList.add('hide-play-button');
+    videoButton.classList.add('hide-play-button');
     videoTitle.classList.add('hide-video-title');
     videoCounter.classList.add('hide-video-counter');
   } else if (!video.seeking) {
@@ -45,16 +45,15 @@ videoPlayButton.forEach(item => item.addEventListener('click', playAndPause));
 // * Detect if video is paused
 function isPaused() {
   // Hide controls unless user is seeking
-  const parentElement = this.parentNode;
-
+  const videoWrapper = this.parentElement;
+  const videoTitle = videoWrapper.querySelector('.video-title');
+  const videoButton = videoWrapper.querySelector('.play-button');
+  const videoCounter = videoWrapper.querySelector('.video-counter');
   const videoPaused = this;
-  const videoTitle = parentElement.childNodes[1];
-  const videoIcon = parentElement.childNodes[5];
-  const videoCounter = parentElement.childNodes[7];
 
   if (!videoPaused.seeking) {
     videoPaused.controls = false;
-    videoIcon.classList.remove('hide-play-button');
+    videoButton.classList.remove('hide-play-button');
     videoTitle.classList.remove('hide-video-title');
     videoCounter.classList.remove('hide-video-counter');
   }
