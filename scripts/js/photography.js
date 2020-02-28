1102adc3a9ff205064e07a487dce6b46d2b13c61
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-28 14:21:43 +0000

    Photography page wipe effect

--- scripts/js/photography.js
+++ scripts/js/photography.js
@@ -3,34 +3,33 @@
 // *=========================================
 
 // Main timeline
-const tlOne = gsap.timeline({
-  defaults: { duration: 0.5, ease: Back.easeOut.config(1) },
-});
+// const tlOne = gsap.timeline({
+//   defaults: { duration: 0.5, ease: Back.easeOut.config(1) },
+// });
 
-tlOne
-  .fromTo(
-    '.half-width-photo-image-one',
-    { clipPath: 'inset(0% 100% 0% 0%)' },
-    { clipPath: 'inset(0% 0% 0% 0%)' },
-  )
-  .fromTo(
-    '.half-width-photo-image-two',
-    { clipPath: 'inset(0% 100% 0% 0%)' },
-    { clipPath: 'inset(0% 0% 0% 0%)' },
-  );
+// tlOne
+//   .fromTo(
+//     '.half-width-photo-image-one',
+//     { clipPath: 'inset(0% 100% 0% 0%)' },
+//     { clipPath: 'inset(0% 0% 0% 0%)' },
+//   )
+//   .fromTo(
+//     '.half-width-photo-image-two',
+//     { clipPath: 'inset(0% 100% 0% 0%)' },
+//     { clipPath: 'inset(0% 0% 0% 0%)' },
+//   );
 
 // * Init ScrollMagic
 const controller = new ScrollMagic.Controller();
+const photoImage = document.querySelectorAll('.photo-image');
 
-const photoWrapper = document.querySelectorAll('.photo-wrapper');
-
-photoWrapper.forEach(function(elem) {
+photoImage.forEach(function(elem) {
   const sceneOne = new ScrollMagic.Scene({
     triggerElement: elem,
     triggerHook: 0.4,
     // reverse: false,
   })
-    .setTween(tlOne)
+    .setClassToggle(elem, 'photo-image-reveal')
     .addIndicators({
       name: 'photo',
       colorTrigger: '#f00',
