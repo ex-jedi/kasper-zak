27099e80856ff5a1243b96c29fda5d7c6048a957
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-02-29 18:14:35 +0000

    Working parallax

--- scripts/js/photography.js
+++ scripts/js/photography.js
@@ -8,14 +8,27 @@ const photoImage = document.querySelectorAll('.photo-image');
 
 photoImage.forEach(function(elem) {
   const sceneOne = new ScrollMagic.Scene({
-    triggerElement: elem,
+    triggerElement: elem.parentElement,
     triggerHook: 0.4,
-    // reverse: false,
   })
     .setClassToggle(elem, 'photo-image-reveal')
     .addIndicators({
       name: 'photo',
       colorTrigger: '#f00',
     })
+    .addTo(controller);
+});
+
+// * Parallax
+const parallaxTl = gsap.timeline();
+parallaxTl.from(photoImage, 2, { y: '-75px', ease: 'none' }, 0);
+
+photoImage.forEach(function(elem) {
+  const slideParallaxScene = new ScrollMagic.Scene({
+    triggerElement: elem.parentElement,
+    triggerHook: 0.2,
+    duration: '100%',
+  })
+    .setTween(parallaxTl)
     .addTo(controller);
 });
