a50628efd4c1ef920b5b3490547844e6f3dd8d44
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-03-25 16:50:22 +0000

    Photography JS file tidy

--- scripts/minified/commercial-work-min.js
+++ scripts/minified/commercial-work-min.js
@@ -1,2 +1,2 @@
-console.log("Hi there");const controller=new ScrollMagic.Controller,commercialImage=document.querySelectorAll(".commercial-image");commercialImage.forEach((function(e){new ScrollMagic.Scene({triggerElement:e.closest(".commercial-image-wrapper"),triggerHook:.5}).setClassToggle(e,"commercial-image-reveal").addTo(controller)}));
+const controller=new ScrollMagic.Controller,commercialImage=document.querySelectorAll(".commercial-image");commercialImage.forEach((function(e){new ScrollMagic.Scene({triggerElement:e.closest(".commercial-image-wrapper"),triggerHook:.5}).setClassToggle(e,"commercial-image-reveal").addTo(controller)}));
 //# sourceMappingURL=commercial-work-min.js.map
