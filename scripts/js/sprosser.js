62ed2e1c06a9c97609b5f71f762fab1fee2c6060
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-07 18:52:15 +0100

    Updates and tweaks

--- scripts/js/sprosser.js
+++ scripts/js/sprosser.js
@@ -5,10 +5,20 @@ console.clear();
 // Get play button and videos
 const playButton = document.querySelector('.play-button');
 const allVideos = document.querySelectorAll('.sprosser-video');
+const videoOne = document.querySelector('.video-one');
+const videoTwo = document.querySelector('.video-two');
+const videoThree = document.querySelector('.video-three');
+const videoFour = document.querySelector('.video-four');
 
 // Play button click handler
-function playButtonClickHandler(e) {
-  console.log(e);
+function playButtonClickHandler() {
+  allVideos.forEach(videos => {
+    if (videos.paused || videos.ended) {
+      videos.play();
+    } else {
+      videos.pause();
+    }
+  });
 }
 
 // Add event listener to button
