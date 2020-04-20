c3ce47eec17315df2163e1cd7ccb85023f601849
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-20 17:35:40 +0100

    Working play and pause function

--- scripts/js/sprosser.js
+++ scripts/js/sprosser.js
@@ -6,6 +6,8 @@ console.clear();
 // Get play button and videos
 
 const playButton = document.querySelector('.play-button');
+const pauseButton = document.querySelector('.pause-button');
+const videoButtonsWrapper = document.querySelector('.play-button-wrapper');
 const allVideos = document.querySelectorAll('.sprosser-video');
 
 // Play button click handler
@@ -13,11 +15,28 @@ function playButtonClickHandler() {
   allVideos.forEach(videos => {
     if (videos.paused || videos.ended) {
       videos.play();
-    } else {
-      videos.pause();
+      playButton.classList.add('hide-play-and-pause-button');
+      playButton.addEventListener(
+        'transitionend',
+        () => {
+          playButton.style.display = 'none';
+          pauseButton.style.display = 'block';
+        },
+        { once: true }
+      );
     }
+  });
+}
+
+function pauseButtonClickHandler() {
+  allVideos.forEach(videos => {
+    pauseButton.style.display = 'none';
+    playButton.style.display = 'block';
+    setTimeout(() => playButton.classList.remove('hide-play-and-pause-button'), 100);
+    videos.pause();
   });
 }
 
 // Add event listener to button
 playButton.addEventListener('click', playButtonClickHandler);
+pauseButton.addEventListener('click', pauseButtonClickHandler);
