aaa2811378d6867040e96a3d3e270ac9f949b139
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-28 18:09:06 +0100

    Adding text fade in to about page

--- perch/templates/layouts/about-footer-end-js.php
+++ perch/templates/layouts/about-footer-end-js.php
@@ -1,5 +1,9 @@
 <!-- TODO: Change Vue to production version or delete -->
-<script type="text/javascript" src="/scripts/minified/vue.js" ></script>
+<script type="text/javascript" src="/scripts/minified/vue.js"></script>
+<!-- ScrollMagic Files -->
+<script src="/scripts/minified/ScrollMagic.min.js"></script>
+<!-- TODO: Remove Indicators -->
+<script src="/scripts/minified/debug.addIndicators.min.js"></script>
 <!-- General JS file -->
 <script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
 <!-- Page JS file  -->
