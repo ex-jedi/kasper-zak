d2d7c3fc5d5a205794a51744c31e29139add874f
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-28 18:33:46 +0100

    Removing ScrollMagic indicators

--- perch/templates/layouts/about-footer-end-js.php
+++ perch/templates/layouts/about-footer-end-js.php
@@ -1,10 +1,8 @@
 <!-- ScrollMagic Files -->
 <script src="/scripts/minified/ScrollMagic.min.js"></script>
-<!-- TODO: Remove Indicators -->
-<script src="/scripts/minified/debug.addIndicators.min.js"></script>
 <!-- General JS file -->
 <script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
 <!-- Page JS file  -->
-<script src="/scripts/js/about.js"></script>
+<script src="/scripts/minified/about-min.js"></script>
 </body>
 </html>
