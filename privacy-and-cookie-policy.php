248010a0d341bee722a0eec4be7f558fb54fd1df
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-04-28 11:20:01 +0100

    Terms and privact pages to universal footer

--- privacy-and-cookie-policy.php
+++ privacy-and-cookie-policy.php
@@ -1,7 +1,8 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
 <?php perch_layout('main-header'); ?>
-	<div class="page-wrapper">
-		<main class="legals privacy-policy" id="main-content">
-        <?php perch_content('Privacy And Cookie Policy'); ?>
-		</main>
-		<?php perch_layout('main-footer'); ?>
+<div class="page-wrapper">
+	<main class="legals privacy-policy" id="main-content">
+			<?php perch_content('Privacy And Cookie Policy'); ?>
+	</main>
+<?php perch_layout('universal-footer'); ?>
+<?php perch_layout('universal-footer-end-js'); ?>
--- terms.php
+++ terms.php
@@ -1,7 +1,8 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
 <?php perch_layout('main-header'); ?>
-	<div class="page-wrapper">
-		<main class="legals terms-and conditions">
-        <?php perch_content('Terms And Conditions'); ?>
-		</main>
-		<?php perch_layout('main-footer'); ?>
+<div class="page-wrapper">
+	<main class="legals terms-and conditions">
+			<?php perch_content('Terms And Conditions'); ?>
+	</main>
+<?php perch_layout('universal-footer'); ?>
+<?php perch_layout('universal-footer-end-js'); ?>
