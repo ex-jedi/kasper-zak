f87c8500f33c9c03a4bc0815f0140e61553880b9
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-06-03 14:45:37 +0100

    Tidying installations.php file

--- installations.php
+++ installations.php
@@ -3,8 +3,6 @@
 <main class="main-content installations-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
 	<?php perch_content("Installations Links"); ?>
-	<!-- <?php perch_layout("installations-links"); ?> -->
-	<!-- <?php perch_layout("svg-divider"); ?> -->
 </main>
 <?php perch_layout('universal-footer'); ?>
 <?php perch_layout('universal-footer-end-js'); ?>
