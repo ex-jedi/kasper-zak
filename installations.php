f05da084a6c054a52b0c7b4ea9824575b46798fa
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-06-03 14:42:30 +0100

    Changing installations link to HTML template

--- installations.php
+++ installations.php
@@ -2,8 +2,9 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content installations-main-content" id="main-content">
 	<?php perch_content("Page Title"); ?>
-	<?php perch_layout("installations-links"); ?>
-	<?php perch_layout("svg-divider"); ?>
+	<?php perch_content("Installations Links"); ?>
+	<!-- <?php perch_layout("installations-links"); ?> -->
+	<!-- <?php perch_layout("svg-divider"); ?> -->
 </main>
 <?php perch_layout('universal-footer'); ?>
 <?php perch_layout('universal-footer-end-js'); ?>
