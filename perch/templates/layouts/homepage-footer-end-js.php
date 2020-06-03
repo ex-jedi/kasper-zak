6d9cd6d72631d10f0b611bbf72edc4f1c398446f
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-06-03 11:57:17 +0100

    Tidying homepage footer js links

--- perch/templates/layouts/homepage-footer-end-js.php
+++ perch/templates/layouts/homepage-footer-end-js.php
@@ -2,17 +2,12 @@
 <script src="/scripts/minified/ScrollMagic.min.js"></script>
 <!-- ScrollMagic Greensock  -->
 <script src="/scripts/minified/animation.gsap.min.js"></script>
-<!-- TODO: Remove Indicators -->
-<script src="/scripts/minified/debug.addIndicators.min.js"></script>
 <!-- Greensock -->
 <script src="/scripts/minified/gsap.min.js"></script>
 <!-- General JS file -->
 <!-- Reframe  -->
 <script src="/scripts/minified/reframe.js"></script>
-<script type="text/javascript" src="/scripts/minified/universal-min.js"></script>
 <!-- Homepage JS File  -->
-<!-- TODO: Link to minified version -->
-<!-- <script src="/scripts/minified/homepage-min.js"></script> -->
-<script src="/scripts/js/homepage.js"></script>
+<script src="/scripts/minified/homepage-min.js"></script>
 </body>
 </html>
