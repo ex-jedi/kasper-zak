6a70b6ca3dc707dff8cca5ad8f237f3a98c7b034
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-11 11:26:14 +0100

    Adding sitemap

--- sitemap.php
+++ sitemap.php
@@ -5,19 +5,16 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 perch_pages_navigation(array(
      'template' => 'sitemap.html',
+     'add-trailing-slash' => true,
      'flat' => true,
      'hide-extensions' => true
  ));
-     perch_blog_custom(array(
-     'template' => 'sitemap-blog.html',
-     'sort'=>'postDateTime',
-     'sort-order'=>'DESC',
-     'count' => 3000
- ));
- perch_blog_categories(array(
-     'sort' => 'catSlug',
-     'sort=order'=> 'ASC',
-     'template'=> 'sitemap-category.html'
+perch_pages_navigation(array(
+     'navgroup' =>'secondary-links',
+     'template' => 'secondary-links-sitemap.html',
+     'add-trailing-slash' => true,
+     'flat' => true,
+     'hide-extensions' => true
  ));
  echo '</urlset>';
  ?>
