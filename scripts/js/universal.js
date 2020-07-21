f1b807f3820842558915cdb96e39704f3764e125
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-07-21 21:35:03 +0100

    New cookie warning

--- scripts/js/universal.js
+++ scripts/js/universal.js
@@ -122,3 +122,28 @@ window.addEventListener('scroll', function() {
   // saves the new position for iteration.
   scrollPos = document.body.getBoundingClientRect().top;
 });
+
+// *=========================================
+// ** Cookie Warning  **
+// *=========================================
+
+const cookieBanner = document.querySelector('.cookie-warning-wrapper');
+const cookieWarningButton = document.querySelector('.cookie-warning-button');
+
+if (localStorage.getItem('cookieSeen') !== 'shown') {
+  cookieBanner.classList.add('show-cookie-warning');
+} else {
+  cookieBanner.style.display = 'none';
+}
+
+cookieWarningButton.addEventListener(
+  'click',
+  () => {
+    localStorage.setItem('cookieSeen', 'shown');
+    cookieBanner.classList.remove('show-cookie-warning');
+    cookieBanner.addEventListener('transitionend', () => {
+      cookieBanner.style.display = 'none';
+    });
+  },
+  { once: true }
+);
