0d6f91ee8892d8d53e5213c7eb25e80e00ab1726
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-11-22 12:20:02 +0000

    Commercial image swipe triggered individually

--- scripts/minified/commercial-work-min.js
+++ scripts/minified/commercial-work-min.js
@@ -1,2 +1,2 @@
-const controller=new ScrollMagic.Controller,commercialImage=document.querySelectorAll(".commercial-image");commercialImage.forEach((function(e){new ScrollMagic.Scene({triggerElement:e.closest(".commercial-image-wrapper"),triggerHook:.8}).setClassToggle(e,"commercial-image-reveal").addTo(controller)}));
+const controller=new ScrollMagic.Controller,commercialImage=document.querySelectorAll(".commercial-image");commercialImage.forEach((function(e){new ScrollMagic.Scene({triggerElement:e,triggerHook:.8}).setClassToggle(e,"commercial-image-reveal").addTo(controller)}));
 //# sourceMappingURL=commercial-work-min.js.map
